cea9720905eca599242ee033f757891bd2474dcd
Rory <rory88@gmail.com>
2015-11-10 12:58:10 +0000

    migrated add page to `add` route

--- client/app/add/add.controller.js
+++ client/app/add/add.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('booktradeBootstrapApp')
-  .controller('AddCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AddCtrl', function ($scope, $http) {
+    $scope.userInputBook = '';
+
+    $scope.books = [];
+
+    $scope.bookLookup = function () {
+      console.log('Doing a book lookup for ', $scope.userInputBook);
+
+      $http.get('/api/books/' + $scope.userInputBook).success(function (data) {
+        console.log(data);
+        $scope.books = data;
+      }).error(function (error) {
+        console.log(error);
+      });
+    }
+
   });
