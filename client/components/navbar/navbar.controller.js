e16d3f9d775f8200f4738e8b00fc46ca06216bb3
Rory <rory88@gmail.com>
2015-11-10 16:05:06 +0000

    changed 'home' to 'all books'

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('booktradeBootstrapApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'All books',
       'link': '/'
     },
       {
