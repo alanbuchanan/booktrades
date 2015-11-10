70b1a2d0b262104266f351c8335ce59c3e5a950a
Rory <rory88@gmail.com>
2015-11-10 17:57:28 +0000

    moved `get` process to its own function. called this function again when user deletes: inefficient?

--- client/app/my-books/my-books.controller.js
+++ client/app/my-books/my-books.controller.js
@@ -6,19 +6,26 @@ angular.module('booktradeBootstrapApp')
     $scope.getCurrentUser = Auth.getCurrentUser().name;
     $scope.books = [];
 
-    $http.get('/api/books').success(function (books) {
-      books.forEach(function (book) {
-        if (book.owner === $scope.getCurrentUser) {
-          $scope.books.push(book)
-        }
-      });
-      console.log('Books: ', books);
-    }).error(function (error) {
-      console.log('There was a problem: ', error);
-    })
+    var getBooks = function () {
+      $http.get('/api/books').success(function (books) {
+        books.forEach(function (book) {
+          if (book.owner === $scope.getCurrentUser) {
+            $scope.books.push(book)
+          }
+        });
+        console.log('Books: ', books);
+      }).error(function (error) {
+        console.log('There was a problem: ', error);
+      })
+    };
 
+    getBooks();
+
+    // Deletes a book from my-books
     $scope.removeMyBook = function (book) {
       $http.delete('/api/books/' + book.id).success(function () {
+        $scope.books = [];
+        getBooks();
 
       }).error(function (error) {
         console.log(error);
