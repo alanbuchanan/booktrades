230bb7e29729b94ef02f6315a784b3bacf3f2fdf
Rory <rory88@gmail.com>
2015-11-11 21:40:50 +0000

    added further to `trade` view and made some todos

--- client/app/trade/trade.controller.js
+++ client/app/trade/trade.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('booktradeBootstrapApp')
-  .controller('TradeCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('TradeCtrl', function ($scope, $http) {
+
+    $scope.trades = [];
+
+    $http.get('/api/trades').success(function (trades) {
+      $scope.trades = trades;
+      console.log($scope.trades);
+    }).error(function (error) {
+      console.log('There was an error:', error);
+    })
   });
+
+//TODO: think of a way to layout numerous trade requests
+//TODO: filter $scope.trades so that it's relative to the user
+//TODO: perform get request to get book info from /api/books using id
+//TODO: implement accept or reject functionality
