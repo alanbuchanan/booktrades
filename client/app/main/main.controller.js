d565be96f39e677595cc0490f99128fe855bf542
Rory <rory88@gmail.com>
2015-11-11 03:47:08 +0000

    experimented with trade dialog

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,10 +13,13 @@ angular.module('booktradeBootstrapApp')
     })
   });
 
-// TODO: experiment with adding a book to books collection bearing in mind notes made
+// TODO: fix custom dialog, fix console error that comes up when you click trade in `my-books`. ideally move route to a separate file
+// TODO: Trades
 // TODO: prevent user interacting with app if they are not logged in. always redirect to login
 // TODO: can trades view be done in an alert with dropdowns?
 // TODO: add material toast for UX when they add a book
 // TODO: message if nothing returned from 'add' search
 // tODO: loading spinner for 'add' search
 // TODO: improve footer on every ng-repeat item in `my-books` and `all books`
+// TODO: why is scss showing errors in the 'messages' pane every time you create a route
+// TODO: delete trade-dialog route if unneeded, from VCS as well
--- client/app/my-books/my-books.controller.js
+++ client/app/my-books/my-books.controller.js
@@ -23,6 +23,41 @@ angular.module('booktradeBootstrapApp')
 
     getBooks();
 
+    // User wants to trade a book
+    $scope.trade = function (userClickedBook) {
+      console.log('hi from trade');
+      $mdDialog.show({
+        controller: TradeDialogCtrl,
+        template:
+        '<md-dialog aria-label="Trade" ng-cloak>' +
+          '<form>' +
+            '<md-toolbar>' +
+              '<div class="md-toolbar-tools">' +
+                '<h2>Trade books with someone else</h2>' +
+                '<span flex></span>' +
+                '<md-button class="md-icon-button" ng-click="cancel()">' +
+                  '<md-icon md-svg-src="img/icons/ic_close_24px.svg" aria-label="Close dialog"></md-icon>' +
+                '</md-button>' +
+              '</div>' +
+            '</md-toolbar>' +
+            '<md-dialog-content style="max-width:800px;max-height:810px; ">' +
+              '<div class="md-dialog-content">' +
+                '<p>Stuff</p>' +
+              '</div>' +
+            '</md-dialog-content>' +
+          '</form>' +
+        '</md-dialog>',
+        parent: angular.element(document.body),
+        targetEvent: userClickedBook,
+        clickOutsideToClose: true
+      })
+        .then(function (answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function () {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
+
     // Deletes a book from my-books
     $scope.removeMyBook = function (book) {
 
@@ -31,14 +66,14 @@ angular.module('booktradeBootstrapApp')
         .content('')
         .ok('Ok')
         .cancel('Cancel');
-      $mdDialog.show(confirm).then(function() {
+      $mdDialog.show(confirm).then(function () {
         httpDelete();
-      }, function() {
+      }, function () {
         // error
       });
 
 
-    var httpDelete = function () {
+      var httpDelete = function () {
         $http.delete('/api/books/' + book.id).success(function () {
           // Get books from db (again) to prevent dupes on front end: inefficient?
           getBooks();
@@ -49,3 +84,18 @@ angular.module('booktradeBootstrapApp')
     }
 
   });
+
+function TradeDialogCtrl($scope, $mdDialog) {
+
+  $scope.aogpiowirecgmoeisrgmcoes = 'Yes';
+
+  $scope.hide = function () {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function () {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function (answer) {
+    $mdDialog.hide(answer);
+  };
+}
