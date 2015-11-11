e406321cf9771239a742a115cd53ba82969312ad
Rory <rory88@gmail.com>
2015-11-11 19:58:13 +0000

    implemented new mechanism for popup involving passing vals to trade-dialog controller, and using a separate view rather than attempting to keep it all in the same controller.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,21 +1,58 @@
+
 'use strict';
-
 angular.module('booktradeBootstrapApp')
-  .controller('MainCtrl', function ($scope, $http, Auth) {
+// User wants to trade a book
+.controller('MainCtrl', function ($scope, $http, Auth, $mdDialog) {
 
-    $scope.getCurrentUser = Auth.getCurrentUser().name;
+
+    $scope.getCurrentUser = Auth.getCurrentUser;
 
     $scope.books = [];
+    $scope.usersBooks = [];
 
+
+    // Get list of all books from db
     $http.get('/api/books').success(function (books) {
       $scope.books = books;
+
+      // Get users books
+      $scope.books.forEach(function (book) {
+        if (book.owner === $scope.getCurrentUser().name) {
+          $scope.usersBooks.push(book);
+        }
+      })
+
       console.log('Books: ', books);
     }).error(function (error) {
       console.log('There was a problem: ', error);
-    })
+    });
 
-    $scope.isUsers= function (book) {
-      return $scope.getCurrentUser === book.owner;
+    $scope.currentTradeBook = {};
+
+    // User pressed trade button
+    $scope.trade = function (ev, userClickedBook) {
+
+      $scope.currentTradeBook = userClickedBook;
+
+      $mdDialog.show({
+        controller: 'TradeDialogCtrl',
+        templateUrl: 'app/trade-dialog/trade-dialog.html',
+        clickOutsideToClose:true,
+        locals: {
+          userClickedBook: userClickedBook,
+          usersBooks: $scope.usersBooks
+        }
+      })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
+
+    // Check if book belongs to user
+    $scope.isUsers = function (book) {
+      return $scope.getCurrentUser().name === book.owner;
     }
   });
 
