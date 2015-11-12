4f7f1eaffcf1fb1aeed506c8454ad528718abd82
Rory <rory88@gmail.com>
2015-11-12 17:46:09 +0000

    improved `trade` process and design

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -51,20 +51,15 @@ angular.module('booktradeBootstrapApp')
         });
     };
 
-    // Check if book belongs to user
-    $scope.isUsers = function (book) {
-      return $scope.getCurrentUser().name === book.owner;
-    }
   });
 
 // TODO: fix schema for book trade
-// TODO: fix custom dialog, fix console error that comes up when you click trade in `my-books`. ideally move route to a separate file
-// TODO: Trades
 // TODO: prevent user interacting with app if they are not logged in. always redirect to login
-// TODO: can trades view be done in an alert with dropdowns?
 // TODO: add material toast for UX when they add a book
-// TODO: message if nothing returned from 'add' search
-// tODO: loading spinner for 'add' search
-// TODO: improve footer on every ng-repeat item in `my-books` and `all books`
+// TODO: If 0 trade requests in the `trade-requests` view, show a different message
+// TODO: loading spinner for 'add' search and every get request across site
 // TODO: why is scss showing errors in the 'messages' pane every time you create a route
-// TODO: delete trade-dialog route if unneeded, from VCS as well
+// TODO: improve footer on every ng-repeat item in `my-books` and `all books`
+// TODO: message if nothing returned from 'add' search
+// TODO: Fix top menu: it goes two-tier at a certain browser width
+//
--- client/app/trade/trade.controller.js
+++ client/app/trade/trade.controller.js
@@ -15,7 +15,7 @@ angular.module('booktradeBootstrapApp')
 
         // Trades should be composed of wanted username of trades from db
         trades.forEach(function (trade) {
-          if (trade.wanted.user === $scope.getCurrentUser().name) {
+          if (trade.wanted.owner === $scope.getCurrentUser().name) {
             $scope.trades.push(trade);
           }
         })
@@ -39,13 +39,8 @@ angular.module('booktradeBootstrapApp')
     $scope.acceptTrade = function (trade) {
 
       // Perform owner swap in `books`
-      $scope.wantedUserTemp = trade.wanted.user;
-      $scope.offeredUserTemp = trade.offered.user;
-
-      var swapBook = {
-        bookId: trade.offered.bookId,
-        user: $scope.wantedUserTemp
-      }
+      $scope.wantedUserTemp = trade.wanted.owner;
+      $scope.offeredUserTemp = trade.offered.owner;
 
       $http.put('/api/books', {bookId: trade.offered.id, owner: $scope.wantedUserTemp})
         .success(function () {
@@ -87,9 +82,16 @@ angular.module('booktradeBootstrapApp')
     //  console.log('problem:', error);
     //})
 
+    // Check if book belongs to user
+    $scope.isUsers = function (book) {
+      return $scope.getCurrentUser().name === book.owner;
+    }
+
   });
 
+//TODO: prevent duplicate trades!
 //TODO: amend `books` model and post reqs to remove `tradeRequests` property
 //TODO: perform get request to get book info from /api/books using id
 //TODO: prevent any book in a current trade from being possible to trade: 'this book cannot be traded because it is part of an active trade'
 //TODO: improve get request in this file: don't get all trades and then foreach, search db by username instead
+//
