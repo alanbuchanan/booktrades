2dc4f48e9793e03b36cafaf4e635bbbb6da6e9d5
Rory <rory88@gmail.com>
2015-11-13 17:06:27 +0000

    added toast

--- client/app/my-books/my-books.controller.js
+++ client/app/my-books/my-books.controller.js
@@ -1,7 +1,41 @@
 'use strict';
 
 angular.module('booktradeBootstrapApp')
-  .controller('MyBooksCtrl', function ($scope, $http, Auth, $mdDialog) {
+  .controller('MyBooksCtrl', function ($scope, $http, Auth, $mdDialog, $mdToast) {
+
+    // Toast
+    var last = {
+      bottom: true,
+      top: false,
+      left: false,
+      right: true
+    };
+
+    $scope.toastPosition = angular.extend({},last);
+    $scope.getToastPosition = function() {
+      sanitizePosition();
+      return Object.keys($scope.toastPosition)
+        .filter(function(pos) { return $scope.toastPosition[pos]; })
+        .join(' ');
+    };
+
+    function sanitizePosition() {
+      var current = $scope.toastPosition;
+      if ( current.bottom && last.top ) current.top = false;
+      if ( current.top && last.bottom ) current.bottom = false;
+      if ( current.right && last.left ) current.left = false;
+      if ( current.left && last.right ) current.right = false;
+      last = angular.extend({},current);
+    }
+
+    $scope.showSimpleToast = function() {
+      $mdToast.show(
+        $mdToast.simple()
+          .content('Your book was successfully deleted!')
+          .position($scope.getToastPosition())
+          .hideDelay(3000)
+      );
+    };
 
     $scope.getCurrentUser = Auth.getCurrentUser().name;
     $scope.books = [];
@@ -33,6 +67,7 @@ angular.module('booktradeBootstrapApp')
         .cancel('Cancel');
       $mdDialog.show(confirm).then(function () {
         httpDelete();
+        $scope.showSimpleToast();
       }, function () {
         // error
       });
